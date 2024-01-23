30cd9f2ea8fd97e3b0e6d6689a23b2531ac05382
Agathius <p.akos1148@gmail.com>
2024-01-23 01:00:41 +0100

    added custom tailwind style as a plugin: focus visible style; added color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,28 @@
-/** @type {import('tailwindcss').Config} */
+/** @type {import("tailwindcss").Config} */
+
+import customStyles from "./tailwind-custom-styles"; // Adjust the path as needed
+
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        color_1: "#121212",
+        color_2: "#1e1e1e",
+        color_3: "#a9a9a9",
+        color_4: "#e1e1e1",
+        color_5: "#4b5563",
+        color_6: "#3b3b3b",
+        color_accent: "#67ad7c",
+        color_accent_secondary: "#6f957b",
+      },
+      fontFamily: {
+        roboto: ["Roboto", "sans-serif"],
+      },
+    },
   },
-  plugins: [],
+  plugins: [customStyles],
 }
