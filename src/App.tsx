4880f0a4ac0e6b7b5ba064c90ae7a0af1d292bfd
Agathius <p.akos1148@gmail.com>
2024-01-24 23:10:19 +0100

    prettified components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 // Main Layout
-import {lazy, Suspense} from 'react';
+import {lazy, Suspense} from "react";
 import NavigationMenu from "./components/NavigationMenu";
 import MainContent from "./components/MainContent";
-import LoaderMenu from './components/LoaderMenu';
-import MainTitleAndDashboardContent from './components/UI/MainTitleAndDashboardContent';
+import LoaderMenu from "./components/LoaderMenu";
+import MainTitleAndDashboardContent from "./components/UI/MainTitleAndDashboardContent";
 import useToggleMenu from "./hooks/useToggleMenu";
 
 // LAZY LOAD
@@ -14,8 +14,7 @@ function App() {
   const {isToggled} = useToggleMenu();
 
   return (
-    <div className="flex flex-col w-full min-h-dvh lg:h-screen lg:p-[2rem] overflow-y-auto lg:overflow-hidden md:bg-color_1 lg:bg-color_5"
-    >
+    <div className="flex flex-col w-full min-h-dvh lg:h-screen lg:p-[2rem] overflow-y-auto lg:overflow-hidden md:bg-color_1 lg:bg-color_5">
       <div className="grid grid-cols-1 lg:grid-cols-[56px,1fr,1.5fr] grid-flow-row lg:h-[calc(100vh-2*2rem)] w-full xl:w-[1200px] self-center rounded overflow-hidden bg-color_1 gap-5 mt-[56px] lg:m-0">
         {isToggled && (
           <Suspense fallback={<LoaderMenu/>}>
--- src/main.tsx
+++ src/main.tsx
@@ -2,24 +2,24 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import {ClientsProvider} from "./context/clientContext.tsx";
-import { FormProvider } from "./context/formContext.tsx";
-import { ToggleMenuProvider } from "./context/toggleMenuContext.tsx";
-import { PropertyProvider } from "./context/propertyContext.tsx";
-import { BrowserRouter } from "react-router-dom";
+import {FormProvider} from "./context/formContext.tsx";
+import {ToggleMenuProvider} from "./context/toggleMenuContext.tsx";
+import {PropertyProvider} from "./context/propertyContext.tsx";
+import {BrowserRouter} from "react-router-dom";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-    <ToggleMenuProvider>
-    <ClientsProvider>
-    <PropertyProvider>
-    <FormProvider>
-      <App/>
-    </FormProvider>  
-    </PropertyProvider>
-    </ClientsProvider>
-    </ToggleMenuProvider>
+      <ToggleMenuProvider>
+        <ClientsProvider>
+          <PropertyProvider>
+            <FormProvider>
+              <App/>
+            </FormProvider>  
+          </PropertyProvider>
+        </ClientsProvider>
+      </ToggleMenuProvider>
     </BrowserRouter>
   </React.StrictMode>,
 )
