45cfbfce145af4b08a3631838ed85bcab1c6e7d4
Agathius <p.akos1148@gmail.com>
2024-02-27 23:25:01 +0100

    updated vite config to handle api routes

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,21 @@ import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      "/api/v1/clients": {
+        target: "http://localhost:8001",
+        changeOrigin: true,
+        secure: false,
+        ws: true,
+      },
+      "/api/v1/properties": {
+        target: "http://localhost:8001",
+        changeOrigin: true,
+        secure: false,
+        ws: true,
+      },
+    },
+  },
   plugins: [react()],
 })
