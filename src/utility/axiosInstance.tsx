cb93fa37fbc37e62c5f970e9676a6a6477d46f85
Agathius <p.akos1148@gmail.com>
2024-03-02 23:48:04 +0100

    added type to axios instance

--- src/utility/axiosInstance.tsx
+++ src/utility/axiosInstance.tsx
@@ -1,9 +1,10 @@
 // Define axios instance specifying the proper endpoints
 import axios from "axios"
+import { AxiosInstance } from "axios"
 
-const axiosInstance = axios.create({
-  baseURL: "http://localhost:8001/api/v1/",
-  timeout: 5000, // Timeout in milliseconds
+const axiosInstance: AxiosInstance = axios.create({
+  baseURL: "http://localhost:8001/api/v1/", // TODO: Replace with hosted url
+  timeout: 5000,
   headers: {
     "Content-Type": "application/json",
   },
