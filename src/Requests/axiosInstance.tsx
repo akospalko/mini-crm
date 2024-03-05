401d03e637051e9df7d2eb984e7a0493c8d96061
Agathius <p.akos1148@gmail.com>
2024-03-05 23:38:56 +0100

    fixed url

--- src/Requests/axiosInstance.tsx
+++ src/Requests/axiosInstance.tsx
@@ -3,7 +3,7 @@ import axios from "axios"
 import { AxiosInstance } from "axios"
 
 const axiosInstance: AxiosInstance = axios.create({
-  baseURL: "http://localhost:8001/api/v1/", // TODO: Replace with hosted url
+  baseURL: "/api/v1/", // TODO: Replace with hosted url
   timeout: 5000,
   headers: {
     "Content-Type": "application/json",
