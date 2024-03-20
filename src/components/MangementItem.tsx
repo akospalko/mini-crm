57f079a71427dc6b2cf251324a380eb44ad720cf
Agathius <p.akos1148@gmail.com>
2024-03-20 23:12:59 +0100

    Reworked Property show handler: separated it from the reusable edit handler fn, now it gets data from api.

--- src/components/MangementItem.tsx
+++ src/components/MangementItem.tsx
@@ -1,49 +1,62 @@
 // Client item for management page
-import useClients from "../hooks/useClients"
-import useForm from "../hooks/useForm"
-import useFormDataTemplate from "../hooks/useFormDataTemplate"
-import useToggleMenu from "../hooks/useToggleMenu"
-import useProperty from "../hooks/useProperty"
+import useClients from "../hooks/useClients";
+import useForm from "../hooks/useForm";
+import useFormDataTemplate from "../hooks/useFormDataTemplate";
+import useToggleMenu from "../hooks/useToggleMenu";
+import useProperty from "../hooks/useProperty";
 import {
   ACTIVE_MANAGEMENT,
   ACTIVE_MENU_ACTION_TYPE,
-  DATABASE_RESOURCES,
-} from "../types/actionTypes"
+  REDUCER_ACTION_TYPE_CLIENT,
+  REDUCER_ACTION_TYPE_PROPERTY,
+} from "../types/actionTypes";
 import {
   ClientItemI,
   PopulateFormDataWithActiveClientI,
   PropertyItemI,
   ClientManagementPropsI,
-} from "../types/types"
+  ClientPropertyI,
+} from "../types/types";
 import {
-  deleteItemByID,
   findActiveArrayItem,
   populateFormWithActiveData,
-} from "../utility/misc"
-import ManagementItemButton from "./UI/ManagementItemButton"
-import { DeleteIcon, EditIcon } from "./UI/SVG"
-import dataAction from "../data/data_test_id.json"
-import testID from "../data/data_test_id.json"
-import text from "../data/text.json"
+} from "../utility/misc";
+import ManagementItemButton from "./UI/ManagementItemButton";
+import { DeleteIcon, EditIcon } from "./UI/SVG";
+import dataAction from "../data/data_test_id.json";
+import testID from "../data/data_test_id.json";
+import text from "../data/text.json";
+import {
+  deleteClient,
+  deleteProperty,
+  getAllClients,
+  getAllProperties,
+} from "../Requests/apiRequests";
+import { getPropertyById } from "../Requests/apiRequests";
 
-const MangementItem = ({ itemData, activeManagement }: ClientManagementPropsI) => {
+const MangementItem = ({
+  itemData,
+  activeManagement,
+}: ClientManagementPropsI) => {
   // CONTEXT
   const {
     clients,
     dispatch: dispatchClients,
     REDUCER_ACTIONS_CLIENT,
-  } = useClients()
+  } = useClients();
+
   const {
     property,
     dispatch: dispatchProperty,
     REDUCER_ACTIONS_PROPERTY,
-  } = useProperty()
-  const { toggleModal, menuContentChangeHandler } = useToggleMenu()
-  const { setFormData } = useForm()
+  } = useProperty();
+
+  const { toggleModal, menuContentChangeHandler } = useToggleMenu();
+  const { setFormData } = useForm();
 
   // HOOK
   const { getClientFormTemplate, getPropertyFormTemplate } =
-    useFormDataTemplate()
+    useFormDataTemplate();
 
   // UTIL
   // Remap activeClients nested "properties" entries -> make form compatible
@@ -51,64 +64,95 @@ const MangementItem = ({ itemData, activeManagement }: ClientManagementPropsI) =
     activeClientData: ClientItemI
   ): PopulateFormDataWithActiveClientI => {
     if (!activeClientData) {
-      return {}
+      return {};
     }
 
     const updatedActiveClient: PopulateFormDataWithActiveClientI = {
       ...activeClientData,
-    }
+    };
 
     if (activeClientData.properties && activeClientData.properties.length > 0) {
       activeClientData.properties.forEach((property) => {
         if (property.id) {
-          updatedActiveClient[property.id] = property.value
+          updatedActiveClient[property.id] = property.value;
         }
-      })
+      });
 
       // remove "properties" entry
-      delete updatedActiveClient.properties
+      delete updatedActiveClient.properties;
     }
-
-    return updatedActiveClient
-  }
+    return updatedActiveClient;
+  };
 
   // HANDLERS
   // Delete client and property item
-  const deleteItemHandler = (isClientAction: boolean): void => {
-    // check for provided arguments
-    if (
-      isClientAction === undefined ||
-      isClientAction === null ||
-      typeof isClientAction !== "boolean"
-    ) {
-      throw new Error(`${text["error-invalid-argument"]} ${isClientAction}`)
+  const deleteItemHandler = async (id: string): Promise<void> => {
+    const invalidIDError: string = "Invalid id";
+    if (!id) {
+      throw new Error(invalidIDError);
     }
+    try {
+      if (id.charAt(0) === "c") {
+        await deleteClient(id);
+        const { responseData } = await getAllClients();
+        dispatchClients({
+          type: REDUCER_ACTION_TYPE_CLIENT.UPDATE_CLIENT,
+          payload: { clients: responseData as ClientItemI[] },
+        });
+      } else if (id.charAt(0) === "p") {
+        await deleteProperty(id);
+        const { responseData } = await getAllProperties();
+        dispatchProperty({
+          type: REDUCER_ACTION_TYPE_PROPERTY.UPDATE_PROPERTY,
+          payload: { property: responseData as PropertyItemI[] },
+        });
+        // TODO: Refetch property
+        // TODO: Update property
+      }
+    } catch (err) {
+      throw new Error(`Error while deleting item: ${err}`);
+    }
+  };
 
-    // delete item from array
-    const updatedItemsArray: ClientItemI[] | PropertyItemI[] = deleteItemByID(
-      itemData.id,
-      isClientAction ? clients : property
-    )
+  //------------------------------------
+  // NEW
+  const showClientHandler = (id: string) => {};
 
-    // update state using the chosen dispatch function
-    isClientAction
-      ? dispatchClients({
-          type: REDUCER_ACTIONS_CLIENT.DELETE_CLIENT,
-          payload: { clients: updatedItemsArray as ClientItemI[] },
-        })
-      : dispatchProperty({
-          type: REDUCER_ACTIONS_PROPERTY.DELETE_PROPERTY,
-          payload: { property: updatedItemsArray as PropertyItemI[] },
-        })
+  // Show property menu
+  // TODO: Fix types
+  const showPropertyHandler = async (id: string) => {
+    if (!id || typeof id !== "string") {
+      throw new Error(`Missing id: ${id}`);
+    }
+    try {
+      // Fetch data
+      const propertyData = await getPropertyById(id);
+      const { responseData } = propertyData;
+      // Set up active menu content
+      menuContentChangeHandler(ACTIVE_MENU_ACTION_TYPE.EDIT_PROPERTY);
+      // Update active property state
+      dispatchProperty({
+        type: REDUCER_ACTIONS_PROPERTY.UPDATE_ACTIVE_PROPERTY,
+        payload: { activeProperty: responseData as PropertyItemI },
+      });
 
-    // update ls
-    localStorage.setItem(
-      isClientAction
-        ? DATABASE_RESOURCES.CLIENTS
-        : DATABASE_RESOURCES.PROPERTIES,
-      JSON.stringify(updatedItemsArray)
-    )
-  }
+      // Populate form
+      const populatedForm = populateFormWithActiveData(
+        responseData,
+        getPropertyFormTemplate()
+      );
+      // Update form state with populated data
+      setFormData(populatedForm);
+      // Open modal
+      toggleModal(true);
+    } catch (error) {
+      // Handle errors
+      console.error(`Error fetching property item (${id}):`, error);
+    }
+  };
+
+  // ----------------------------
+  // OLD
 
   // Edit/update client and property item
   const editItemHandler = (isClientAction: boolean) => {
@@ -118,7 +162,7 @@ const MangementItem = ({ itemData, activeManagement }: ClientManagementPropsI) =
       isClientAction === null ||
       typeof isClientAction !== "boolean"
     ) {
-      throw new Error(`${text["error-invalid-argument"]} ${isClientAction}`)
+      throw new Error(`${text["error-invalid-argument"]} ${isClientAction}`);
     }
 
     // set up active menu content
@@ -126,13 +170,13 @@ const MangementItem = ({ itemData, activeManagement }: ClientManagementPropsI) =
       isClientAction
         ? ACTIVE_MENU_ACTION_TYPE.EDIT_CLIENT
         : ACTIVE_MENU_ACTION_TYPE.EDIT_PROPERTY
-    )
+    );
 
     // find activec item
     const activeItem: ClientItemI | PropertyItemI = findActiveArrayItem(
       itemData.id,
       isClientAction ? clients : property
-    )
+    );
 
     // update state
     isClientAction
@@ -143,62 +187,63 @@ const MangementItem = ({ itemData, activeManagement }: ClientManagementPropsI) =
       : dispatchProperty({
           type: REDUCER_ACTIONS_PROPERTY.UPDATE_ACTIVE_PROPERTY,
           payload: { activeProperty: activeItem as PropertyItemI },
-        })
+        });
 
     // process active item: client -> assign properties to form, property -> same as activeItem
     const activeItemProcessed:
       | PopulateFormDataWithActiveClientI
       | PropertyItemI = isClientAction
       ? getActiveClientWithRemappedProperties(activeItem as ClientItemI)
-      : (activeItem as PropertyItemI)
+      : (activeItem as PropertyItemI);
 
     // populate form
     const populatedForm = populateFormWithActiveData(
       activeItemProcessed,
       isClientAction ? getClientFormTemplate() : getPropertyFormTemplate()
-    )
+    );
 
     // update form state with populated data
-    setFormData(populatedForm)
+    setFormData(populatedForm);
 
     // open modal
-    toggleModal(true)
-  }
+    toggleModal(true);
+  };
 
   // STYLE
-  const iconSize: string = "25px"
-  const iconColor: string = "var(--color_1)"
-  const contentStyle: string = "my-auto truncate"
+  const iconSize: string = "25px";
+  const iconColor: string = "var(--color_1)";
+  const contentStyle: string = "my-auto truncate";
 
   // LAYOUT
   // Get active content
-  let activeEditItemHandler = () => {}
-  let activeDeleteItemHandler = () => {}
-  const clientItemData: ClientItemI = itemData as ClientItemI
-  const propertyItemData: PropertyItemI = itemData as PropertyItemI
+  let activeEditHandler = async () => {};
+  // let activeDeleteItemHandler = () => {}
+  // let activeDeleteItemHandler = deleteItemHandler(itemData.id)
+  const clientItemData: ClientItemI = itemData as ClientItemI;
+  const propertyItemData: PropertyItemI = itemData as PropertyItemI;
 
-  let itemContent
+  let itemContent;
   switch (activeManagement) {
     case ACTIVE_MANAGEMENT.CLIENT_MANAGEMENT:
-      activeEditItemHandler = () => editItemHandler(true)
-      activeDeleteItemHandler = () => deleteItemHandler(true)
+      activeEditHandler = () => editItemHandler(true);
+      // activeDeleteItemHandler = () => deleteItemHandler(true)
       itemContent = (
         <>
           <span
-            title={clientItemData["full name"]}
+            title={clientItemData["full_name"]}
             className={`${contentStyle} mr-5`}
           >
-            {clientItemData["full name"]}
+            {clientItemData["full_name"]}
           </span>
           <span title={clientItemData["position"]} className={contentStyle}>
             {clientItemData["position"]}
           </span>
         </>
-      )
-      break
+      );
+      break;
     case ACTIVE_MANAGEMENT.PROPERTY_MANAGEMENT:
-      activeEditItemHandler = () => editItemHandler(false)
-      activeDeleteItemHandler = () => deleteItemHandler(false)
+      activeEditHandler = async () => await showPropertyHandler(itemData.id);
+      // activeDeleteItemHandler = () => deleteItemHandler(itemData.id)
       itemContent = (
         <>
           <span
@@ -211,11 +256,13 @@ const MangementItem = ({ itemData, activeManagement }: ClientManagementPropsI) =
             {propertyItemData.type}
           </span>
         </>
-      )
-      break
+      );
+      break;
     default:
-      throw new Error(`text["error-unknown-action-type"] ${activeManagement}`)
+      throw new Error(`text["error-unknown-action-type"] ${activeManagement}`);
   }
+
+  // RENDER
 
   return (
     <div
@@ -226,19 +273,19 @@ const MangementItem = ({ itemData, activeManagement }: ClientManagementPropsI) =
       <ManagementItemButton
         title={text["title-edit"]}
         dataAction={dataAction["data-action-edit"]}
-        changed={activeEditItemHandler}
+        clicked={activeEditHandler}
       >
         <EditIcon width={iconSize} height={iconSize} fill={iconColor} />
       </ManagementItemButton>
       <ManagementItemButton
         title={text["title-delete"]}
         dataAction={dataAction["data-action-delete"]}
-        changed={activeDeleteItemHandler}
+        clicked={() => deleteItemHandler(itemData.id)}
       >
         <DeleteIcon width={iconSize} height={iconSize} fill={iconColor} />
       </ManagementItemButton>
     </div>
-  )
-}
+  );
+};
 
-export default MangementItem
+export default MangementItem;
