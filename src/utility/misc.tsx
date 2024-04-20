645aa638599d192d123df80bd9fff7e932f73386
Palko Akos <palko.akos@p4it.hu>
2024-04-20 22:35:35 +0200

    Added reusable process form data util. Removed unused utils.

--- src/utility/misc.tsx
+++ src/utility/misc.tsx
@@ -1,74 +1,4 @@
 // Miscellaneous utils
-import {
-  ClientItemI,
-  PropertyItemI,
-  ClientFormTemplateI,
-  PopulateFormDataWithActiveClientI,
-  PropertyFormTemplateI,
-} from "../types/types"
-
-// Find clicked item in an array of items for Clients and Properties
-export const findActiveArrayItem = (
-  itemID: string,
-  data: (ClientItemI | PropertyItemI)[]
-): ClientItemI | PropertyItemI => {
-  const filteredItem: ClientItemI | PropertyItemI =
-    data?.find((item) => itemID === item.id) ??
-    ({} as ClientItemI | PropertyItemI)
-
-  return filteredItem
-}
-
-// Remove item from array return new array
-export const deleteItemByID = (
-  itemID: string,
-  data: ClientItemI[] | PropertyItemI[]
-): ClientItemI[] | PropertyItemI[] => {
-  const filteredArray = data.filter(
-    (item: ClientItemI | PropertyItemI) => item.id !== itemID
-  ) as ClientItemI[] | PropertyItemI[]
-
-  return filteredArray
-}
-
-// Populate form data with active item's data
-export const populateFormWithActiveData = (
-  activeData: PropertyItemI | PopulateFormDataWithActiveClientI,
-  formTemplate: ClientFormTemplateI | PropertyFormTemplateI
-) => {
-  const populatedForm = { ...formTemplate }
-
-  if (activeData) {
-    Object.keys(populatedForm).forEach((key) => {
-      const typedKey = key as keyof (
-        | PropertyItemI
-        | PopulateFormDataWithActiveClientI
-      )
-
-      if (activeData[typedKey] !== undefined && activeData[typedKey] !== null) {
-        populatedForm[typedKey].value = activeData[typedKey] as string | boolean
-      }
-    })
-  }
-
-  return populatedForm
-}
-
-// Update data array with the provided (e.g. updated) obj: used for clients and properties
-export const updateDataArray = (
-  originalArray: ClientItemI[] | PropertyItemI[],
-  updatedObject: ClientItemI | PropertyItemI
-): ClientItemI[] | PropertyItemI[] => {
-  const updatedArray = originalArray.map((item) => {
-    if (item.id === updatedObject.id) {
-      // if the ids match, replace the item with the updated object
-      return updatedObject
-    }
-    return item
-  }) as ClientItemI[] | PropertyItemI[]
-
-  return updatedArray
-}
 
 // Format obj keys to labels
 export const objKeysToLabels = (
@@ -82,4 +12,23 @@ export const objKeysToLabels = (
     )
   }
   return formattedText
+}
+
+// Process form data for client and property (create / update) api requests
+export const processFormData = (formData: any) => {
+  const processedFormData:any = {};
+  for (const key in formData) {
+    if (formData.hasOwnProperty(key)) {
+      const entry = formData[key]; 
+      if (entry.hasOwnProperty("propertyId") && typeof entry.propertyId === "string" && entry.propertyId.trim() !== "") {
+        processedFormData[key] = {
+          propertyId: entry.propertyId,
+          value: entry.value
+        };
+      } else {
+        processedFormData[key] = entry.value;
+      }
+    }
+  }
+  return processedFormData;
 }
